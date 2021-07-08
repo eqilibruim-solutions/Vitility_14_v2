e3b3e0835ccfdade00c348e8d373a537fef072ac
Business Agility Masters <39414107+BusinessAgilityMasters@users.noreply.github.com>
2021-07-08 11:14:27 +0200

    upload Sale order line stock

---
+++ sale_order_line_stock/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Business Agility Masters
+#    Copyright (C) 2018-Today Trey, Kilobytes de Soluciones <www.trey.es>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Sale order line stock',
+    'summary': 'Sale order line stock',
+    'description': '''
+Add a field in sale order line to show quantity on hand in selected warehouse and amount of products in package.
+''',
+    'author': 'Busines Agility Masters',
+    'website': 'https://www.businessagilitymasters.nl',
+    'category': 'Sales',
+    'version': '10.0.0.1.1',
+    'depends': [
+        'sale_stock',
+        'stock',
+    ],
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'installable': True,
+}
---
+++ sale_order_line_stock/models/sale_order_line.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, fields, api
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    wh_qty_available = fields.Char(
+        compute='_compute_wh_qty_available',
+        string='Quantity on hand warehouse')
+
+    @api.one
+    @api.depends(
+        'product_id', 'order_id.warehouse_id', 'product_id.immediately_usable_qty')
+    def _compute_wh_qty_available(self):
+        if self.product_id.exists() and self.product_id.x_omdoos_aantal_producten:
+            product = self.env['product.product'].with_context(
+                warehouse=self.order_id.warehouse_id.id).browse(
+                self.product_id.id)
+            self.wh_qty_available = str(int(product.immediately_usable_qty)) + "(" + \
+                                str(product.x_omdoos_aantal_producten) + ")"
+        elif self.product_id.exists():
+            product = self.env['product.product'].with_context(
+                warehouse=self.order_id.warehouse_id.id).browse(
+                self.product_id.id)
+            self.wh_qty_available = str(int(product.immediately_usable_qty))
