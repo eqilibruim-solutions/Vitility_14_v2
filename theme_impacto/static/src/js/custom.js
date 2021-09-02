151d099ed5aaf1f736f6d6e1dabac07ccaa6fa02
Business Agility Masters <39414107+BusinessAgilityMasters@users.noreply.github.com>
2021-09-02 14:55:09 +0200

    Update Version 10

--- theme_impacto/static/src/js/custom.js
+++ theme_impacto/static/src/js/custom.js
@@ -2,10 +2,11 @@
 /*************************
       Predefined Variables
 *************************/ 
-var COMERCIO = {};
+var IMPACTO = {};
 
  (function($){
-  "use strict"; 
+  "use strict";
+
 /*************************
       Predefined Variables
 *************************/ 
@@ -28,8 +29,8 @@ var COMERCIO = {};
  /*************************
        counter
 *************************/  
- COMERCIO.counters = function () {
-     var $counter = $('.counter-box');
+ IMPACTO.counters = function () {
+     var $counter = $('.counter');
           if ($counter.exists()) {
               $counter.each(function () {
                   var $elem = $(this);
@@ -44,7 +45,7 @@ var COMERCIO = {};
 /*************************
        owl-carousel 
 *************************/
- COMERCIO.carousel = function () {
+ IMPACTO.carousel = function () {
     $(".owl-carousel").each(function () {
         var $this = $(this),
             $items = ($this.data('items')) ? $this.data('items') : 1,
@@ -52,10 +53,10 @@ var COMERCIO = {};
             $navdots = ($this.data('nav-dots')) ? $this.data('nav-dots') : false,
             $navarrow = ($this.data('nav-arrow')) ? $this.data('nav-arrow') : false,
             $autoplay = ($this.attr('data-autoplay')) ? $this.data('autoplay') : true,
-            $space = ($this.attr('data-space')) ? $this.data('space') : 30;       
+            $space = ($this.attr('data-space')) ? $this.data('space') : 30;		    
            
             $(this).owlCarousel({
-                loop: false,
+				        loop: false,
                 mouseDrag: false,
                 touchDrag: false,
                 pullDrag: false,
@@ -81,7 +82,7 @@ var COMERCIO = {};
 /*************************
        Magnific Popup
 ************************/
-COMERCIO.mediaPopups = function () {
+IMPACTO.mediaPopups = function () {
     if ($(".popup-img").exists()) {
           $('.popup-img').magnificPopup({
               type: 'image',
@@ -115,91 +116,126 @@ COMERCIO.mediaPopups = function () {
 /*************************
        Progressbar
 *************************/  
-// COMERCIO.progressBar = function () {
-//    var $progressBar = $('.progress-bar');
-//     if ($progressBar.exists()) {
-//         $progressBar.each(function (i, elem) {
-//             var $elem = $(this),
-//                 percent = $elem.attr('data-percent') || "100",
-//                 delay = $elem.attr('data-delay') || "100",
-//                 type = $elem.attr('data-type') || "%";
-//             if (!$elem.hasClass('progress-animated')) {
-//                 $elem.css({
-//                     'width': '0%'
-//                 });
-//             }
-//         var progressBarRun = function () {
-//             $elem.animate({
-//                 'width': percent + '%'
-//             }, 'easeInOutCirc').addClass('progress-animated');
-//              $elem.delay(delay).append('<span class="progress-type animated fadeIn">' + type + '</span><span class="progress-number animated fadeIn">' + percent + '</span>');
-//         };
-//         $(elem).appear(function () {
-//                 setTimeout(function () {
-//                     progressBarRun();
-//                 }, delay);
-//             });
-//         });
-//       }
-// };
-
- 
+IMPACTO.progressBar = function () {
+   var $progressBar = $('.progress-bar');
+    if ($progressBar.exists()) {
+        $progressBar.each(function (i, elem) {
+            var $elem = $(this),
+                percent = $elem.attr('data-percent') || "100",
+                delay = $elem.attr('data-delay') || "100",
+                type = $elem.attr('data-type') || "%";
+            if (!$elem.hasClass('progress-animated')) {
+                $elem.css({
+                    'width': '0%'
+                });
+            }
+        var progressBarRun = function () {
+            $elem.animate({
+                'width': percent + '%'
+            }, 'easeInOutCirc').addClass('progress-animated');
+             $elem.delay(delay).append('<span class="progress-type animated fadeIn">' + type + '</span><span class="progress-number animated fadeIn">' + percent + '</span>');
+        };
+        $(elem).appear(function () {
+                setTimeout(function () {
+                    progressBarRun();
+                }, delay);
+            });
+        });
+      }
+};
 
 /*************************
-         Product Details
+  Rounded Skill
 *************************/
-COMERCIO.product_details = function () {
-     $("#pro_detail_zoom.owl-carousel").owlCarousel({
-            items: 1,
-            margin: 30,
-            navigation: true,
-            pagination: false
-    })
-    $("#o-carousel-product .carousel-indicators").mCustomScrollbar();
-    $('.menu-filter').click(function() {
-      $("#products_grid_before").css({
-          "width": "300px",
-          "transition": "0.5s"
-      });
-      $("#wrapwrap").css({
-          "margin-left": "300px",
-          "transition": "0.5s"
-      });
-     $('body').css("overflow-x", "hidden"); 
-     $('.popup-overlay').addClass('active');
-  });
-    $('.popup-overlay').click(function() {
-       $(this).removeClass('active');
-         $("#products_grid_before").css({
-            "width": "0",
-            "transition": "0.5s"
-        });
-         $("#wrapwrap").css({
-          "margin-left": "0",
-          "transition": "0.5s"
-      });
-    });
+IMPACTO.roundedSkill = function () {
+      var $roundedSkillEl = $('.rounded-skill');
+      if( $roundedSkillEl.length > 0 ){
+        $roundedSkillEl.each(function(){
 
-  $('.mobile-view-filter-close-btn').click(function() {
-      $("#products_grid_before").css({
-          "width": "0px",
-          "transition": "0.5s"
-      });
-      $("#wrapwrap").css({
-          "margin-left": "0px",
-          "transition": "0.5s"
-      });
-      $(".main-header-maxW").removeClass("transparent");
-  });
+          var element = $(this);
+          var roundSkillSize = element.attr('data-size');
+          var roundSkillSpeed = element.attr('data-speed');
+          var roundSkillWidth = element.attr('data-width');
+          var roundSkillColor = element.attr('data-color');
+          var roundSkillTrackColor = element.attr('data-trackcolor');
+          if( !roundSkillSize ) { roundSkillSize = 140; }
+          if( !roundSkillSpeed ) { roundSkillSpeed = 2000; }
+          if( !roundSkillWidth ) { roundSkillWidth = 8; }
+          if( !roundSkillColor ) { roundSkillColor = '#0093BF'; }
+		     if( !roundSkillTrackColor ) { roundSkillTrackColor = 'rgba(200,200,200,0.2)'; }
+          var properties = {roundSkillSize:roundSkillSize, roundSkillSpeed:roundSkillSpeed, roundSkillWidth:roundSkillWidth, roundSkillColor:roundSkillColor, roundSkillTrackColor:roundSkillTrackColor};
+           element.css({'width':roundSkillSize+'px','height':roundSkillSize+'px','line-height':roundSkillSize+'px'}).animate({opacity:0}, 10);
+            element.appear(function () {
+              if (!element.hasClass('skills-animated')) {
+                var t = setTimeout( function(){ element.css({opacity: 1}); }, 100 );
+                runRoundedSkills( element, properties );
+                element.addClass('skills-animated');
+              }
+            });
+        });      
 
-
+       }
+    function runRoundedSkills( element, properties){
+      element.easyPieChart({
+        size: Number(properties.roundSkillSize),
+        animate: Number(properties.roundSkillSpeed),
+        scaleColor: false,
+        trackColor: properties.roundSkillTrackColor,
+        lineWidth: Number(properties.roundSkillWidth),
+        lineCap: 'square',
+        barColor: properties.roundSkillColor
+      });
+    }
 }
 
 
 /*************************
+         Masonry
+*************************/
+IMPACTO.masonry = function () {
+    var $masonry = $('.masonry-main .masonry'),
+      $itemElement = '.masonry-main .masonry-item';
+      if ($masonry.exists()) {
+        $masonry.isotope({
+          resizable: true,
+          itemSelector: $itemElement,
+          masonry: {
+            gutterWidth: 10
+          }
+        });
+     }  
+}
+
+
+/*************************
+         Isotope
+*************************/
+IMPACTO.Isotope = function () {
+       var $isotope = $(".isotope"),
+          $itemElement = '.grid-item',
+          $filters = $('.isotope-filters');      
+        if ($isotope.exists()) {
+            $isotope.isotope({
+            resizable: true,
+            itemSelector: $itemElement,
+              masonry: {
+                gutterWidth: 10
+              }
+            });     
+       $filters.on( 'click', 'button', function() {
+         var $val = $(this).attr('data-filter');
+             $isotope.isotope({ filter: $val });       
+             $filters.find('.active').removeClass('active');
+             $(this).addClass('active');
+      });     
+    }
+
+}
+
+/*************************
   Search Box
 *************************/
-COMERCIO.searchbox = function (e) {
+IMPACTO.searchbox = function (e) {
     $('a[href="#search-but"]').on("click", function(event) {
       event.preventDefault();
       $("#search-box").addClass("open");
@@ -220,201 +256,22 @@ COMERCIO.searchbox = function (e) {
   });
 }
 
-
-/*************************
-  Search Box
-*************************/
-COMERCIO.shoppageview = function () {
-    $(".new-filter .shift_list_view").click(function(e) {
-        $(".new-filter button").removeClass('active');
-        $(this).addClass('active');
-        $('#products_grid').removeClass("list-view-box").removeClass("grid-view-box");
-        $('#products_grid').addClass("list-view-box");        
-        localStorage.setItem("product_view", "list");
-
-    });
-    $(".new-filter .shift_grid_view").click(function(e) {
-       $(".new-filter button").removeClass('active');
-        $(this).addClass('active')
-        $('#products_grid').removeClass("list-view-box").removeClass("grid-view-box");
-        localStorage.setItem("product_view", "grid");
-    });
-
-     $(".new-filter .shift_grid_view2").click(function(e) {
-        $(".new-filter button").removeClass('active');
-        $(this).addClass('active')
-        $('#products_grid').removeClass("list-view-box").removeClass("grid-view-box");
-        $('#products_grid').addClass("grid-view-box");
-        localStorage.setItem("product_view2", "grid2");
-    });
-
-    if (localStorage.getItem("product_view") == 'list') {
-       $(".new-filter button").removeClass('active');
-        $(".new-filter .shift_list_view").addClass('active')
-        $('#products_grid').addClass("list-view-box");
-    }
-    if (localStorage.getItem("product_view") == 'grid') {
-        $(".new-filter button").removeClass('active');
-        $(".new-filter .shift_grid_view").addClass('active')
-        $('#products_grid').removeClass("list-view-box");
-    }   
-}
-
-
-/*************************
-  Pallaraxbanner
-*************************/
-COMERCIO.pallaraxbanner = function () {
-    var lFollowX = 0,
-    lFollowY = 0,
-    x = 0,
-    y = 0,
-    friction = 1 / 30;
-
-    function moveBackground() {
-      x += (lFollowX - x) * friction;
-      y += (lFollowY - y) * friction;
-
-     var translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
-
-      $('.pallarax-move').css({
-        '-webit-transform': translate,
-        '-moz-transform': translate,
-        'transform': translate
-      });
-
-      window.requestAnimationFrame(moveBackground);
-    }
-
-    $("#carousel-1").mousemove(function(e){
-        var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-        var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-        lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-        lFollowY = (10 * lMouseY) / 100;
-    });
-
-    moveBackground();
-}
-
-
-/*************************
-  Pallaraxbanner
-*************************/
-COMERCIO.megamenu_countdown = function () {
-
-  //Mobile menu
-  $(".mm-mega-menu > a").click(function(event) {
-     event.preventDefault();
-     $(this).parent().toggleClass("open-mob-menu");
-     $(this).toggleClass("mob-menu-open");
-  });
-
-
-  $('.countdown').countdown({
-      zeroCallback: function(options) {
-        $('.countdown').fadeOut();
-      }
-    });
-}
-
-
-$(window).scroll(function() {
-    if ($(window).scrollTop() >= 250) {
-        $('body').addClass('sticky-header');
-    } else {
-        $('body').removeClass('sticky-header');
-    }
-});
-
-
-
 /****************************************************
-     COMERCIO Window load and functions
+     IMPACTO Window load and functions
 ****************************************************/
-  
+  //Window load functions
+    $window.load(function () {
+      IMPACTO.masonry(),
+      IMPACTO.Isotope(),	
+      IMPACTO.carousel(),	 
+		  IMPACTO.progressBar();
+    });
 
  //Document ready functions
-    $document.ready(function () {   
-		COMERCIO.carousel();	
-        COMERCIO.counters(), 
-        COMERCIO.mediaPopups(),
-        COMERCIO.searchbox(),
-        COMERCIO.product_details(),
-        COMERCIO.pallaraxbanner(),
-        COMERCIO.megamenu_countdown(),
-        COMERCIO.shoppageview();
-        
-        $('#menu-trigger').click(function(){
-			      $(this).toggleClass('open');
-			      $('#overlay-menu').toggleClass('menu-open');
-		    });
-        $('#ad-carousel-product .owl-thumbs').remove();
-        
-        
-        if($("header").hasClass("head-left"))
-        {
-             $("body").addClass("header-left");
-        }
-        
-        $("header .search_expand, .search_expand, .affixed search_expand").on("click", function(event){
-            $(".search_expand").toggleClass("ct-search-expand");
-            event.stopPropagation();
-        });
-        
-        $('body').on('click', '#search_close', function() {
-       
-            $(".search_expand").removeClass("ct-search-expand");
-            
-        });
-        
-        
-        if($( "header" ).children().hasClass("header-li"))
-        {
-            $( "header" ).addClass("header site-header header-light");
-        }
-        
-        $("body").on('click',".js-copy", function() {
-    			var target_id = $(this).attr("data-content-target");
-    			var target_html = $('#' + target_id).html();
-    			$("#html_data").val(target_html);
-    			$("#sub_data").click();
-		  });
-		  $("body").on("click", ".builder a.nav-link", function(){
-    			var x = $(this).attr("href");
-    			x = x.replace("#", "");
-    			$(".tab-content .tab-pane").each(function(){
-    				var y = $(this).attr("id");
-    				if (x == y) $(this).addClass("active show");
-    				else $(this).removeClass("active show");
-    			});
-      });
+    $document.ready(function () {         
+        IMPACTO.counters(),
+        IMPACTO.roundedSkill(),
+        IMPACTO.mediaPopups(),
+        IMPACTO.searchbox();
     });
-    
- 
-    
-    
-
-    /* Variables */
-    var $verticalCollapsibleMenu = $('#hamburger-menu'),
-        $verticalCollapsibleSubMenu = $verticalCollapsibleMenu.find('.submenu');
-    
-    /* Close Off Canvas Sub Menu */
-    $verticalCollapsibleSubMenu.slideUp();
-
-    /* Category Sub Menu Toggle */
-    $verticalCollapsibleMenu.on('click', 'li a', function(e) {
-        var $this = $(this);
-        if ( $this.siblings('.submenu').length) {
-            e.preventDefault();
-            if ($this.siblings('ul:visible').length){
-                $this.siblings('ul').slideUp();
-            }else {
-                $this.closest('li').siblings('li').find('ul:visible').slideUp();
-                $this.siblings('ul').slideDown();
-            }
-        }
-	  });
 })(jQuery);
-
-
-
